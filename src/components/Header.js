00e2d70e55e8b846eba805805fe049d6584f858c
Alexander Grattan <alex@grattan.me>
2021-01-24 16:14:56 -0500

    fix: working navigation bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,9 +10,9 @@ function Header() {
   const [nav, setNav] = useState(false);
 
   return (
-    <header className="container mx-auto flex w-full justify-between items-start p-4 relative z-30">
-      <img src={logo} alt="pitt-csc-logo" className="w-32 relative" />
-      <nav className="flex flex-col items-end">
+    <header className="container mx-auto w-full p-4 fixed bg-white z-30 md:relative md:bg-none md:flex md:justify-between md:items-center md:text-center">
+      <div className="flex justify-between items-center mx-auto md:block md:mx-0">
+        <img src={logo} alt="pitt-csc-logo" className="w-32 relative" />
         <div
           className={`cursor-pointer md:hidden z-40 
           `}
@@ -24,10 +24,14 @@ function Header() {
             <FontAwesomeIcon icon={faBars} className="text-xl" />
           )}
         </div>
+      </div>
+      <nav
+        className={`transition-all relative bg-white p-4 max-h-0 opacity-0 ${
+          nav ? "max-h-96 opacity-100" : ""
+        } md:max-h-96 md:bg-none md:opacity-100`}
+      >
         <ul
-          className={`flex flex-col justify-center items-end md:flex md:flex-row md:block md:items-center md:space-x-8 font-body ${
-            nav ? "block" : "hidden"
-          }`}
+          className={`flex flex-col justify-center items-center space-y-4 font-body md:flex md:flex-row md:block md:items-center md:space-x-8 md:space-y-0`}
         >
           <li className="font-bold">About Us</li>
           <li className="font-bold">Blog</li>
