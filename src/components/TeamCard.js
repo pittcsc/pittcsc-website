4acd99a04cf0b40a0986a10682b904b0ca78f6d5
Alexander Grattan <alex@grattan.me>
2021-04-24 22:24:41 -0400

    feat: new officers

--- src/components/TeamCard.js
+++ src/components/TeamCard.js
@@ -9,15 +9,13 @@ function TeamCard({ image, name, title, linkedIn, email }) {
   return (
     <figure className="relative bg-gray-100 rounded-2xl p-8 shadow-md w-64 md:w-72 transform transition hover:shadow-lg hover:scale-105">
       <img
-        className="w-48 h-48 object-cover rounded-full -mt-16 mx-auto"
+        className="w-48 h-48 object-cover object-center rounded-full -mt-16 mx-auto"
         src={image}
         alt={`Portrait of ${name}`}
       />
       <figcaption className="text-center font-body">
-        <div className="font-body font-medium text-lg md:text-xl pt-4">
-          {name}
-        </div>
-        <div className="text-sm">{title}</div>
+        <div className="font-medium text-lg md:text-xl pt-4">{name}</div>
+        <div className="text-xs px-2">{title}</div>
       </figcaption>
       <motion.a
         whileHover={{ scale: 1.1 }}
