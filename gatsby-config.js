1054f9cf615059666f07bfb6127703994d597032
Alexander Grattan <alex@grattan.me>
2021-05-11 19:57:52 -0400

    fix: add urls to content security

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,7 +34,7 @@ module.exports = {
       options: {
         headers: {
           "/*": [
-            "Content-Security-Policy: default-src 'self' 'unsafe-inline' data: https://www.google-analytics.com;",
+            "Content-Security-Policy: default-src 'self' 'unsafe-inline' data: https://www.google-analytics.com *.google.com *.hotjar.com; img-src 'self' 'unsafe-inline' *.cloudfront.net; style-src 'self' 'unsafe-inline' *.googleapis.com;",
           ],
         },
       },
