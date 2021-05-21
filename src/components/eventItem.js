c4ed8e5378447e1834f7672d7fdc5c17222cf6a8
Alexander Grattan <alex@grattan.me>
2021-05-21 17:43:28 -0400

    fix: conditional background for tag

--- src/components/eventItem.js
+++ src/components/eventItem.js
@@ -33,7 +33,9 @@ function EventItem({ name, startDate, endDate, url, description, tags }) {
                 <div className="flex space-x-2">
                   {tags.map((tag, i) => (
                     <div
-                      className={`text-sm lg:text-base px-4 py-2 bg-${tag.color}-300 rounded-full`}
+                      className={`text-sm lg:text-base px-4 py-2 ${
+                        tag.color ? `bg-${tag.color}-300` : "bg-gray-300"
+                      } rounded-full`}
                       key={i}
                     >
                       {tag.name}
