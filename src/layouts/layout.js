8bfda29de98d9840e94b63ab47ef41aa724a63d5
GameDog9988 <alex@grattan.me>
2021-07-14 19:44:40 -0400

    feat: tool-version file and fontawesome config

--- src/layouts/layout.js
+++ src/layouts/layout.js
@@ -4,17 +4,23 @@ import PropTypes from "prop-types";
 import { AnimatePresence } from "framer-motion";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
 
-const Layout = ({ title, header, children }) => (
-  <div>
-    <Header title={header} />
-    <SEO title={title} />
-    <AnimatePresence exitBeforeEnter>
-      <main key="main">{children}</main>
-    </AnimatePresence>
-    <Footer />
-  </div>
-);
+const Layout = ({ title, header, children }) => {
+  config.autoAddCss = false;
+
+  return (
+    <div>
+      <Header title={header} />
+      <SEO title={title} />
+      <AnimatePresence exitBeforeEnter>
+        <main key="main">{children}</main>
+      </AnimatePresence>
+      <Footer />
+    </div>
+  );
+};
 
 Layout.propTypes = {
   title: PropTypes.string,
