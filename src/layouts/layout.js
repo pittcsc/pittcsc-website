a3834d955c8e8e3153527119d1766ff6d7be3298
GameDog9988 <alex@grattan.me>
2021-07-14 21:33:35 -0400

    fix: remove font awesome config

--- src/layouts/layout.js
+++ src/layouts/layout.js
@@ -4,12 +4,8 @@ import PropTypes from "prop-types";
 import { AnimatePresence } from "framer-motion";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import { config } from "@fortawesome/fontawesome-svg-core";
-import "@fortawesome/fontawesome-svg-core/styles.css";
 
 const Layout = ({ title, header, children }) => {
-  config.autoAddCss = false;
-
   return (
     <div>
       <Header title={header} />
