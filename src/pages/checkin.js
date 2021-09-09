5785ecc29f8f8f750dc191a271102c41abea8889
Mathew Varughese <mat.varughese3@gmail.com>
2021-09-08 23:20:50 -0400

    Create checkin.js

---
+++ src/pages/checkin.js
@@ -0,0 +1,15 @@
+import React, { useEffect } from "react";
+
+const RedirectPage = ({ data }) => {
+  useEffect(() => {
+    window.location = "location of href"
+  })
+  return (
+    <div>
+    or build a custom sign up form here
+    </div>
+   )
+})
+
+
+export default RedirectPage;
