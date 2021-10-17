58cfbbb2e61f87fc3a18d2f3a10e80b8ecd0910b
GameDog9988 <alex@grattan.me>
2021-10-16 22:26:57 -0400

    fix: remove AnimatePresence import

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion } from "framer-motion";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
