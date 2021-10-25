3a0231bc333797475e4d52db0fe4fc7c343d1126
GameDog9988 <alex@grattan.me>
2021-10-25 07:58:49 -0400

    feat: change sponsor logo animation stagger

--- src/pages/sponsors.js
+++ src/pages/sponsors.js
@@ -19,14 +19,14 @@ const imageContainer = {
   show: {
     opacity: 1,
     transition: {
-      staggerChildren: 0.6,
+      staggerChildren: 0.4,
     },
   },
 };
 
 const item = {
   hidden: { opacity: 0, y: 100 },
-  show: { opacity: 1, y: 0, duration: 0.8 },
+  show: { opacity: 1, y: 0 },
 };
 
 const SponsorPage = ({ data }) => {
