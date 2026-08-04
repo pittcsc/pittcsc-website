e92b453997602d3f58d9b077279a224daef4f6ee
ArchZak <izear21@gmail.com>
2026-08-03 22:29:34 -0400

    feat: created template for initiatives + moved js code to be reuseable

--- src/data/z2oContent.js
+++ src/data/z2oContent.js
@@ -1,7 +1,19 @@
+import Z2O1 from "../images/initiatives/Z2O/z2o-1.png";
+import Z2O2 from "../images/initiatives/Z2O/z2o-2.png";
+import Z2O3 from "../images/initiatives/Z2O/z2o-3.png";
+import Z2O4 from "../images/initiatives/Z2O/z2o-4.png";
+import Z2O5 from "../images/initiatives/Z2O/z2o-5.png";
+import Placements from "../images/initiatives/Z2O/placements.png";
+
 export const z2oContent = {
-  description: `Our 100% student led flagship program to help underclassmen go from "zero" to their first internship offer! This program has helped hundreds of students get their first internship, with a cumulative attendance of over 400. This takes place in the first few weeks of the fall semester.`,
-  workshopsTitle: "Workshops & Events Include:",
-  workshopsList: [
+  title: "Zero to Offer",
+  description: [
+    `Z2O is our 100% student led flagship program to help underclassmen go from "zero" to their first internship offer! This program has helped hundreds of students get their first internship, with a cumulative attendance of over 400. This takes place in the first few weeks of the fall semester.`,
+    `Students can learn about what an internship is, how to max out your resume, ace the interview process, and meet recruiters!`,
+    `From community events, officer led presentations, and an inside scoop in the tech industry, Z2O helps you go from zero to offer! `
+  ],
+  highlightsTitle: "Workshops & Events Include:",
+  highlightsList: [
     "What is an Internship and Why Should You Want One?",
     "Resume Workshop and Reviews",
     "Behavioral Interview Workshop",
@@ -9,6 +21,8 @@ export const z2oContent = {
     "Mock Interview Series",
     "Exclusive Recruiting Events with Corporate Sponsors"
   ],
-  placementsTitle: "Where Our Members Have Received Offers",
-  galleryTitle: "Check out some pictures from our Zero to Offer program!"
+  mainImageTitle: "Where Our Members Have Received Offers",
+  mainImage: Placements,
+  galleryTitle: "Check out some pictures from our Zero to Offer program!",
+  gallery: [Z2O2, Z2O5, Z2O3, Z2O1, Z2O4]
 };
