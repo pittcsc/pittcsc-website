2a0a2570f5c2c3e04e65e8a7a0e2fc186f5483c0
ArchZak <izear21@gmail.com>
2026-08-24 10:59:05 -0400

    fix: typos

--- src/data/cscHacksContent.js
+++ src/data/cscHacksContent.js
@@ -1,7 +1,7 @@
 export const cscHacksContent = {
   title: "CSC Hacks",
   description: [
-    "CSC Hacks challenges teams of up to three students to go as big as possible in just two days. Throughout the event, participants build out their projects and receive guidance from club officers during dedicated work sessions. The initiative concludes with live five-minute demos and Q&A sessions in front of a panel of judges and prizes!"
+    "CSC Hacks challenges teams of up to three students to go as big as possible in just two days. Throughout the event, participants build out their projects and receive guidance from club officers during dedicated work sessions. The initiative concludes with live five-minute demos, Q&A sessions in front of a panel of judges, and prizes!"
   ],
   highlightsTitle: "CSC Hacks Includes:",
   highlightsList: [
