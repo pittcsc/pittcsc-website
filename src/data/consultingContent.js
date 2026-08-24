dd38a5b22aeac91bb274a00bce003d859a3f11e2
ArchZak <izear21@gmail.com>
2026-08-24 10:35:48 -0400

    feat: consulting page

--- src/data/consultingContent.js
+++ src/data/consultingContent.js
@@ -1,6 +1,17 @@
+import CSCC1 from "../images/initiatives/csc-consulting.jpg";
+
 export const consultingContent = {
   title: "Consulting",
   description: [
-    "Details about Consulting are coming soon."
-  ]
+    "Consulting brings together a small team of CSC students who are chosen based on the goal a company is bringing, and the skills needed to carry out that vision. The team then works closely with the company over the semester to build and deliver a product."
+  ],
+  highlightsTitle: "Consulting Includes:",
+  highlightsList: [
+    "Thorough Application and Interview Process",
+    "Real Project Management, System Design, and Software Engineering",
+    "Work that impacts thousands of people",
+  ],
+  mainImageTitle: "The Consulting Technology Team",
+  mainImage: CSCC1,
 };
+
